b029c4360dabc55b68233ab26ff61a6861d60356
Juanma Yegros <juanmayegros@gmail.com>
2023-11-02 14:57:27 -0300

    se agregaron infos en cursos

--- src/secciones/Saltar.tsx
+++ src/secciones/Saltar.tsx
@@ -35,7 +35,7 @@ function Saltar() {
             <p className="mb-2 text-gray-500 dark:text-gray-400">
             Serás acompañado por un instructor experto, quien se encargará de todas las fases del salto, desde la salida hasta el aterrizaje, brindándote una experiencia segura e inolvidable.
             </p>
-            <p className="mb-2 text-gray-500 dark:text-gray-400">INCLUYE</p>
+            <p className="mb-2">INCLUYE</p>
             <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
               <li>
                   Un pintoresco paseo en avión de aproximadamente 25 minutos, donde podrás disfrutar del impresionante paisaje de nuestras ciudades, sus majestuosos cerros y serenos lagos.
@@ -47,7 +47,7 @@ function Saltar() {
                   Un emocionante video capturado de la mano de tu experimentado instructor para revivir cada momento.
               </li>
             </ul>
-            <p className="mb-2 text-gray-500 dark:text-gray-400">COSTO</p>
+            <p className="mb-2">COSTO</p>
             <p className="mb-2 text-gray-500 dark:text-gray-400">
               Esta experiencia única en tu vida está disponible a partir de tan solo 1.350.000 Gs.
             </p>
@@ -68,18 +68,18 @@ function Saltar() {
             
             <Tabs aria-label="Tabs with underline" style="underline">
       <Tabs.Item active title="CURSO ASL / IAD" icon={GiParachute}>
-      <p className="mb-2">Con este método, durante los primeros saltos, tu equipo se abrirá de forma asistida por el 
+      <p className="mb-2 text-gray-500 dark:text-gray-400">Con este método, durante los primeros saltos, tu equipo se abrirá de forma asistida por el 
         instructor o mediante una línea anclada a la aeronave en el momento de la salida.
         </p>
-        <p className="mb-2">Esta técnica permite que tu mente se relaje y se abra a la experiencia del salto, 
+        <p className="mb-2 text-gray-500 dark:text-gray-400">Esta técnica permite que tu mente se relaje y se abra a la experiencia del salto, 
         facilitando una transición gradual y emocionante a la caída libre.
         </p> 
-        <p className="mb-2">Es el punto de partida perfecto para iniciar tu trayectoria como paracaidista profesional. 
+        <p className="mb-2 text-gray-500 dark:text-gray-400">Es el punto de partida perfecto para iniciar tu trayectoria como paracaidista profesional. 
         Aprenderás a ser responsable de cada fase del salto, desde la salida de la aeronave hasta la caída 
         libre y la navegación, con la asistencia constante de un instructor mediante radio para garantizar 
         una experiencia segura y emocionante.
         </p> 
-        <p className="mb-2">¡Descubre la libertad del cielo mientras aprendes con los 
+        <p className="mb-2 text-gray-500 dark:text-gray-400">¡Descubre la libertad del cielo mientras aprendes con los 
         mejores!
         </p>
         <p className="mb-2">INCLUYE</p>
@@ -87,49 +87,140 @@ function Saltar() {
         <span className="font-medium text-gray-800 dark:text-white">Inscripción en la Asociación Paraguaya de Paracaidismo</span>
         </li>
         
-        &emsp;&nbsp;&nbsp;Asegúrate de unirte oficialmente a la comunidad de paracaidistas del Paraguay con nuestra 
+        <p className="mb-2 text-gray-500 dark:text-gray-400">&emsp;&nbsp;&nbsp;Asegúrate de unirte oficialmente a la comunidad de paracaidistas del Paraguay con nuestra 
         asistencia en el proceso de inscripción.
+        </p>
         
         <li>
         Curso Teórico Personalizado (Duración: 8 a 10 horas)
         </li>
-        &emsp;&nbsp;&nbsp;Sumérgete en un curso teórico personalizado, diseñado para proporcionarte conocimientos 
+        <p className="mb-2 text-gray-500 dark:text-gray-400">&emsp;&nbsp;&nbsp;Sumérgete en un curso teórico personalizado, diseñado para proporcionarte conocimientos 
         detallados y esenciales para tu seguridad y éxito como paracaidista.
+        </p>
 
         <li>
         Prácticas y Capacitación Continua
         </li>
-        &emsp;&nbsp;&nbsp;Disfruta de sesiones prácticas intensivas y capacitación continua, 
+        <p className="mb-2 text-gray-500 dark:text-gray-400">&emsp;&nbsp;&nbsp;Disfruta de sesiones prácticas intensivas y capacitación continua, 
         donde mejorarás tus habilidades y ganarás confianza en cada salto.
+        </p>
 
         <li>
         Equipo Completo para la Práctica
         </li>
-        &emsp;&nbsp;&nbsp;Proporcionamos un equipo completo y de alta calidad para tus prácticas, 
+        <p className="mb-2 text-gray-500 dark:text-gray-400">&emsp;&nbsp;&nbsp;Proporcionamos un equipo completo y de alta calidad para tus prácticas, 
         asegurando que estés equipado con lo mejor en tecnología y seguridad.
+        </p>
 
         <li>
         Paseo Escénico en Avión (Duración: Aproximadamente 20 minutos)
         </li>
-        &emsp;&nbsp;&nbsp;Embárcate en un emocionante paseo en avión que te ofrecerá vistas panorámicas 
+        <p className="mb-2 text-gray-500 dark:text-gray-400">&emsp;&nbsp;&nbsp;Embárcate en un emocionante paseo en avión que te ofrecerá vistas panorámicas 
         impresionantes, mientras te preparas para tu salto.
+        </p>
 
         <li>
         Navegación en Paracaídas (Duración: Aproximadamente 7 minutos)
         </li>
-        &emsp;&nbsp;&nbsp;Experimenta la libertad del cielo con aproximadamente 7 minutos de navegación 
+        <p className="mb-2 text-gray-500 dark:text-gray-400">&emsp;&nbsp;&nbsp;Experimenta la libertad del cielo con aproximadamente 7 minutos de navegación 
         en paracaídas, guiado por la suave brisa mientras te deslizas hacia tierra firme.
+        </p>
 
         <li>
         Video de la Salida y el Salto
         </li>
-        &emsp;&nbsp;&nbsp;Capturamos cada momento emocionante, desde tu salida del avión hasta tu 
+        <p className="mb-2 text-gray-500 dark:text-gray-400">&emsp;&nbsp;&nbsp;Capturamos cada momento emocionante, desde tu salida del avión hasta tu 
         aterrizaje, para que puedas revivir tu increíble aventura una y otra vez.
+        </p>
+        <p className="mb-2">¡Haz Realidad una Experiencia Única en Tu Vida!</p>
+        <p className="mb-2 text-gray-500 dark:text-gray-400">
+        Descubre la emoción del paracaidismo a partir de solo 150 USD. Tu aventura inolvidable está a 
+        solo un salto de distancia. ¡No dudes en consultarnos para confirmar el precio exacto y 
+        descubrir nuestras increíbles promociones!
+        </p>
       </Tabs.Item>
       <Tabs.Item title="CURSO AFF" icon={GiCarpetBombing}>
-        This is <span className="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</span>.
-        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
-        control the content visibility and styling.
+      <p className="mb-2 text-gray-500 dark:text-gray-400">
+      Esta modalidad te ofrece la entrada más rápida al mundo del paracaidismo. Desde tu primer salto, 
+      te sumergirás en ejercicios de caída libre, manteniendo tu enfoque en las técnicas y 
+      responsabilidades del salto.
+        </p>
+        <p className="mb-2 text-gray-500 dark:text-gray-400">
+        Perfecto para los amantes de la adrenalina, esta opción te permite experimentar la emoción en su 
+        máxima expresión. Durante todo el proceso, serás guiado por dos instructores expertos que volarán 
+        junto a ti, brindándote asistencia desde la salida hasta el aterrizaje. Aprenderás a manejar cada 
+        fase del salto, desde la salida del avión hasta la apertura del paracaídas, mientras recibes 
+        orientación constante a través de una radio.
+        </p>
+        <p className="mb-2 text-gray-500 dark:text-gray-400">
+        ¡Prepárate para una experiencia intensa y emocionante que te llevará al límite y más allá en 
+        cada salto!
+        </p>
+
+        <p className="mb-2">INCLUYE</p>
+        <li>
+        <span className="font-medium text-gray-800 dark:text-white">Inscripción en la Asociación Paraguaya de Paracaidismo</span>
+        </li>
+        
+        <p className="mb-2 text-gray-500 dark:text-gray-400">&emsp;&nbsp;&nbsp;Asegúrate de ser parte 
+        oficial de la comunidad de paracaidistas del Paraguay con nuestra asistencia en el proceso de 
+        inscripción.
+        </p>
+        
+        <li>
+        Curso Teórico Personalizado (Duración: 8 a 10 horas)
+        </li>
+        <p className="mb-2 text-gray-500 dark:text-gray-400">&emsp;&nbsp;&nbsp;Sumérgete en un curso 
+        teórico detallado y personalizado, diseñado para brindarte conocimientos esenciales para tu 
+        seguridad y éxito como paracaidista.
+        </p>
+
+        <li>
+        Prácticas Intensivas y Capacitación Continua
+        </li>
+        <p className="mb-2 text-gray-500 dark:text-gray-400">&emsp;&nbsp;&nbsp;Disfruta de sesiones 
+        prácticas intensivas y entrenamiento continuo, donde mejorarás tus habilidades y ganarás 
+        confianza en cada salto.
+        </p>
+
+        <li>
+        Equipo Completo para la Práctica
+        </li>
+        <p className="mb-2 text-gray-500 dark:text-gray-400">&emsp;&nbsp;&nbsp;Proporcionamos un equipo 
+        completo y de alta calidad para tus prácticas, asegurando que estés equipado con lo mejor en 
+        tecnología y seguridad.
+        </p>
+
+        <li>
+        Paseo Escénico en Avión (Duración: Aproximadamente 35 minutos)
+        </li>
+        <p className="mb-2 text-gray-500 dark:text-gray-400">&emsp;&nbsp;&nbsp;Disfruta de un emocionante 
+        paseo en avión que te brindará vistas panorámicas espectaculares, mientras te preparas para tu 
+        salto.
+        </p>
+
+        <li>
+        Navegación en Paracaídas (Duración: Aproximadamente 7 minutos)
+        </li>
+        <p className="mb-2 text-gray-500 dark:text-gray-400">&emsp;&nbsp;&nbsp;Experimenta la libertad 
+        del cielo con aproximadamente 7 minutos de navegación en paracaídas, guiado por la suave brisa 
+        mientras te deslizas hacia tierra firme.
+        </p>
+
+        <li>
+        Video de la Salida y el Salto
+        </li>
+        <p className="mb-2 text-gray-500 dark:text-gray-400">&emsp;&nbsp;&nbsp;Capturamos cada momento 
+        emocionante, desde tu salida del avión hasta tu aterrizaje, para que puedas revivir tu increíble 
+        aventura una y otra vez.
+        </p>
+        <p className="mb-2">¡Explora las alturas y descubre el paracaidismo en su máxima expresión con 
+        nuestra experiencia única y emocionante!</p>
+        <p className="mb-2 text-gray-500 dark:text-gray-400">
+        Descubre una experiencia única en tu vida a partir de tan solo 380 USD. Estamos aquí para hacer 
+        realidad tu sueño de volar. ¡Consúltanos para confirmar el precio y descubrir nuestras 
+        promociones exclusivas!
+        </p>
       </Tabs.Item>
     </Tabs>
 
