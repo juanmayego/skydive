146781d6cfdcf0770fcda6a0e369b04cef803568
Juanma Yegros <juanmayegros@gmail.com>
2023-11-02 12:12:13 -0300

    WIP cursos

--- src/secciones/Saltar.tsx
+++ src/secciones/Saltar.tsx
@@ -1,6 +1,8 @@
 
 'use client';
 import { Accordion } from 'flowbite-react';
+import { Tabs } from 'flowbite-react';
+import { GiParachute, GiCarpetBombing } from 'react-icons/gi';
 
 function Saltar() {
   
@@ -64,6 +66,73 @@ function Saltar() {
             ¡Prepárate para volar alto y alcanzar tus sueños en el cielo!
             </p>
             
+            <Tabs aria-label="Tabs with underline" style="underline">
+      <Tabs.Item active title="CURSO ASL / IAD" icon={GiParachute}>
+      <p className="mb-2">Con este método, durante los primeros saltos, tu equipo se abrirá de forma asistida por el 
+        instructor o mediante una línea anclada a la aeronave en el momento de la salida.
+        </p>
+        <p className="mb-2">Esta técnica permite que tu mente se relaje y se abra a la experiencia del salto, 
+        facilitando una transición gradual y emocionante a la caída libre.
+        </p> 
+        <p className="mb-2">Es el punto de partida perfecto para iniciar tu trayectoria como paracaidista profesional. 
+        Aprenderás a ser responsable de cada fase del salto, desde la salida de la aeronave hasta la caída 
+        libre y la navegación, con la asistencia constante de un instructor mediante radio para garantizar 
+        una experiencia segura y emocionante.
+        </p> 
+        <p className="mb-2">¡Descubre la libertad del cielo mientras aprendes con los 
+        mejores!
+        </p>
+        <p className="mb-2">INCLUYE</p>
+        <li>
+        <span className="font-medium text-gray-800 dark:text-white">Inscripción en la Asociación Paraguaya de Paracaidismo</span>
+        </li>
+        
+        &emsp;&nbsp;&nbsp;Asegúrate de unirte oficialmente a la comunidad de paracaidistas del Paraguay con nuestra 
+        asistencia en el proceso de inscripción.
+        
+        <li>
+        Curso Teórico Personalizado (Duración: 8 a 10 horas)
+        </li>
+        &emsp;&nbsp;&nbsp;Sumérgete en un curso teórico personalizado, diseñado para proporcionarte conocimientos 
+        detallados y esenciales para tu seguridad y éxito como paracaidista.
+
+        <li>
+        Prácticas y Capacitación Continua
+        </li>
+        &emsp;&nbsp;&nbsp;Disfruta de sesiones prácticas intensivas y capacitación continua, 
+        donde mejorarás tus habilidades y ganarás confianza en cada salto.
+
+        <li>
+        Equipo Completo para la Práctica
+        </li>
+        &emsp;&nbsp;&nbsp;Proporcionamos un equipo completo y de alta calidad para tus prácticas, 
+        asegurando que estés equipado con lo mejor en tecnología y seguridad.
+
+        <li>
+        Paseo Escénico en Avión (Duración: Aproximadamente 20 minutos)
+        </li>
+        &emsp;&nbsp;&nbsp;Embárcate en un emocionante paseo en avión que te ofrecerá vistas panorámicas 
+        impresionantes, mientras te preparas para tu salto.
+
+        <li>
+        Navegación en Paracaídas (Duración: Aproximadamente 7 minutos)
+        </li>
+        &emsp;&nbsp;&nbsp;Experimenta la libertad del cielo con aproximadamente 7 minutos de navegación 
+        en paracaídas, guiado por la suave brisa mientras te deslizas hacia tierra firme.
+
+        <li>
+        Video de la Salida y el Salto
+        </li>
+        &emsp;&nbsp;&nbsp;Capturamos cada momento emocionante, desde tu salida del avión hasta tu 
+        aterrizaje, para que puedas revivir tu increíble aventura una y otra vez.
+      </Tabs.Item>
+      <Tabs.Item title="CURSO AFF" icon={GiCarpetBombing}>
+        This is <span className="font-medium text-gray-800 dark:text-white">Dashboard tab's associated content</span>.
+        Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to
+        control the content visibility and styling.
+      </Tabs.Item>
+    </Tabs>
+
           </Accordion.Content>
         </Accordion.Panel>
       </Accordion>
